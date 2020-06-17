a4d8c007f54dc4a01c089bfe18108529f68d3384
rodlmoraes <rodlmoraes@hotmail.com>
2020-06-17 17:47:28 -0300

    Booking is now an entity

--- src/main/java/com/gym/lbdgym/model/Booking.java
+++ src/main/java/com/gym/lbdgym/model/Booking.java
@@ -3,12 +3,19 @@ package com.gym.lbdgym.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.gym.lbdgym.model.room.SquashRoom;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@Data
+@Entity
 public class Booking implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/com/gym/lbdgym/model/room/EquipmentRoom.java
+++ src/main/java/com/gym/lbdgym/model/room/EquipmentRoom.java
@@ -18,7 +18,7 @@ public class EquipmentRoom extends Room {
 
     private int maxNumberEquipments;
 
-    @OneToMany(mappedBy = "equipmentRoom")
+    @OneToMany
     @JsonManagedReference(value = "equipmentRoom")
     private List<Equipment> equipments;
 
